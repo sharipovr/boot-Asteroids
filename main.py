68b87ad7fdd05087d1361eb9fb6e43c25d2591ae
sharipovr <sharipov.rustem@gmail.com>
2025-11-21 21:54:33 -0600

    Player object (ship) can rotate in move now

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0  # "delta time", specific for game dev
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -20,11 +20,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)    
         screen.fill("black")
         player.draw(screen)
-        pygame.display.flip()
-
-        dt = clock.tick(60) / 100
+        pygame.display.flip()   # refreshes the screen
+        dt = clock.tick(60) / 1000  # framerate to 60 FPS
         
 
 if __name__ == "__main__":
